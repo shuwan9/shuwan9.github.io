2764638545811fc92bbae6c59e44e47a4a7c904d
shuwan9 <shuwan9@outlook.com>
2020-09-21 09:39:14 +0800

    Update trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-130227707-1`,
+        trackingId: `UA-178473154-1`,
       },
     },
     // {
